7e1d8dc8191925b777a72509368fcdf08173d75e
Egemen Can Uze <egemencanuze@gmail.com>
2017-06-04 17:31:51 +0200

    Add color variables

--- src/components/HomeNavbar.js
+++ src/components/HomeNavbar.js
@@ -1,37 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Navbar,
-  Nav,
-  NavItem
-} from 'react-bootstrap';
 
 class HomeNavbar extends Component {
   render() {
     return (
       <div className="HomeNavbar">
-        <Navbar
-          className="HomeNavbar__Navbar"
-          collapseOnSelect
-          fluid
-        >
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Link to="/">UZE</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Nav pullRight>
-            <NavItem
-              className="HomeNavbar__AboutButton"
-              onClick={() => {
-                this.props.onAboutClick();
-              }}
-            >
-              ABOUT
-            </NavItem>
-          </Nav>
-        </Navbar>
+        <div className="HomeNavbar__Brand">
+          <Link to="/">UZE</Link>
+        </div>
+        <a
+          className="HomeNavbar__AboutButton"
+          onClick={() => {
+            this.props.onAboutClick();
+          }}>
+            ABOUT
+        </a>
       </div>
     );
   }
