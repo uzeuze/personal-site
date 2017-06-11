9bdc18aa2efbdef4486f0f379b5fc2ee776217e0
Egemen Can Uze <egemencanuze@gmail.com>
2017-06-11 14:48:32 +0200

    Change about modals layout

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap';
 
 export default class AboutModal extends Component {
   render() {
     return (
       <div className="AboutModal">
-        <h2>AboutModal</h2>
-        <span onClick={() => this.props.onClose()}>Close</span>
+        <Grid fluid>
+          <Row>
+            <Col md={6} className="AboutModal__info">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste repellendus quae et, consequatur non neque dolor, perspiciatis excepturi molestiae laboriosam soluta sed, harum nulla illum quos nisi repudiandae similique delectus.</p>
+            </Col>
+            <Col md={6} className="AboutModal__message">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis dolor cupiditate repellendus veritatis ipsam labore dolorum voluptates. Deserunt amet praesentium excepturi, facilis tenetur magnam doloremque, harum numquam, hic eius optio.</p>
+            </Col>
+          </Row>
+        </Grid>
+        <span
+          onClick={() => this.props.onClose()}
+          className="AboutModal__close"
+        >
+          Close
+        </span>
       </div>
     );
   }
