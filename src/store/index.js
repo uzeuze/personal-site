76f6cbc7b2f3b416e9fea2c1c893b34821cf5ffc
Egemen Can Uze <egemencanuze@gmail.com>
2017-07-25 00:59:21 +0200

    remove logger and persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { persistStore, autoRehydrate } from 'redux-persist';
 
 import reducers from '../reducers';
-
-//TODO: Add predicate to disable loging in production
-const logger = createLogger();
 
 const store = createStore(
   reducers,
   {},
   compose(
-    applyMiddleware(thunk, logger),
-    autoRehydrate()
+    applyMiddleware(thunk),
   )
 );
-
-persistStore(store, { whitelist: ['auth'] });
 
 export default store;
